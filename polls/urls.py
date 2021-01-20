88f216f9a0056b9987819618b4adca160d133011
Daniele <d.guerrieri@gmail.com>
2021-01-20 18:47:10 +0100

    other small changes

--- polls/urls.py
+++ polls/urls.py
@@ -2,6 +2,17 @@ from django.urls import path
 
 from . import views
 
+app_name = 'polls' # this add namespace to following urls
+
 urlpatterns = [
+    # ex: /polls/
     path('', views.index, name='index'),
+
+    # ex: /polls/5
+    path('<int:question_id>/', views.detail, name='detail'),
+
+    # ex: /polls/5/results
+    path('<int:question_id>/results/', views.results, name='results'),
+
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
